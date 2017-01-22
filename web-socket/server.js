22e713efac0fe33e89a284be9f9d5fce1defff87
Zachary Rosenthal <rosent76@msu.edu>
2017-01-22 03:34:51 -0500

    helll ya got them web sockets

--- web-socket/server.js
+++ web-socket/server.js
@@ -4,31 +4,54 @@ var expressWs = require('express-ws')(app);
 var bodyParser = require('body-parser');
 var data = [];
 
+
+var appW = expressWs.app;
+
+appW.ws('/test', function(ws, req) {
+});
+
+var aWss = expressWs.getWss('/test');
+/*
+setInterval(function () {
+    console.log(data.length);
+    if (data.length !== 0) {
+        var temp = data;
+        data = [];
+        aWss.clients.forEach(function (client) {
+            client.send(temp);
+        });
+    }
+}, 5000);*/
+
+
+
 app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST,  PATCH');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
     console.log('middleware');
     req.testing = 'testing';
     return next();
 });
 
-
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/', function(req, res, next){
-
-    data.concat(req.body);
-
-    console.log('hii: ', data);
-
-    res.end();
+app.get('/', function (req, res) {
+    console.log('hello');
 });
 
-app.ws('/', function(ws, req) {
+app.post('/', function (req, res, next) {
 
-    if (data.length > 0) {
-        var temp = data;
-        data = [];
-        ws.send(temp);
-    }
+    console.log('body: ', req.body);
+
+    jsonData = JSON.stringify(req.body);
+
+    aWss.clients.forEach(function (client) {
+        client.send(jsonData);
+    });
+
 });
 
-app.listen(3000);
+app.listen(3333);
